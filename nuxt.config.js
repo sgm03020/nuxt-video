e753c32e20415c8ba2dbd7e1b42fdbc8595a7a3c
Tsuyoshi Koike <airtk2000@gmail.com>
2020-10-05 23:48:37 +0900

    client-only

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,6 @@ import colors from 'vuetify/es5/util/colors'
 
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
-  mode: 'client',
   head: {
     titleTemplate: '%s - nuxt-video',
     title: 'nuxt-video',
