4e624210f734e47f552d119b41a044fde002c269
Tsuyoshi Koike <airtk2000@gmail.com>
2020-10-06 21:16:01 +0900

    mod index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,58 +1,93 @@
 <template>
   <div>
-    <div>ABCDEFG</div>
+    <div></div>
+    <v-container>
+      <v-row justify="center" align="center" class="pa-0 mb-6">
+        <v-card-title class="justify-center ma-0 pa-0">
+          Welcome to SAM's Video Library
+        </v-card-title>
+      </v-row>
+    </v-container>
     <div>
       <client-only>
-        <carousel-3d
-          v-show="1"
-          :controls-visible="false"
-          :clickable="false"
-          :controls-prev-html="'&#10092;'"
-          :controls-next-html="'&#10093;'"
-          :controls-width="30"
-          :controls-height="60"
-          :width="320"
-          :height="180 + 36 + 0"
-          :onMainSlideClick="void 0"
-          @after-slide-change="onAfterSlideChange"
-        >
-          <slide
-            class="text-center pt-0"
-            style="background-color: black"
-            v-for="(card, i) in cards"
-            :index="i"
-            :key="i"
+        <div class="justify-center text-center">
+          <div
+            v-show="loading"
+            style="
+              width: 100%;
+              height: 216px;
+              margin: 20px 0;
+              z-index: 10;
+              opacity: 0.3;
+            "
+            class="justify-center text-center pa-0"
           >
-            <v-row
-              style="height: 36px; background-color: transparent"
-              dense
-              no-gutters
-              justify="center"
-              align-content="center"
-            >
-              {{ card.title }}
-            </v-row>
-            <!-- Slide 1 Content -->
-            <!-- <img src="https://placehold.it/360x270" /> -->
-            <!-- src="https://img.youtube.com/vi/ZoXdmxYa90c/default.jpg" -->
-            <v-img
-              v-if="i != slideIndex"
+            <img
               width="320"
-              height="180"
-              :lazy-src="require('assets/images/default.jpg')"
-              :src="card.tmb"
-            >
-            </v-img>
-            <LazyYoutubeVideo
-              v-else
-              :src="card.src"
-              :preview-image-size="card.previewImageSize"
-              aspect-ratio="16:9"
-              :thumbnail-listeners="{ load: foo }"
+              height="216"
+              :src="require('assets/images/0.jpg')"
             />
-          </slide>
-        </carousel-3d>
+          </div>
+
+          <carousel-3d
+            v-show="!loading"
+            :controls-visible="false"
+            :clickable="false"
+            :controls-prev-html="'&#10092;'"
+            :controls-next-html="'&#10093;'"
+            :controls-width="30"
+            :controls-height="60"
+            :width="320"
+            :height="180 + 36 + 0"
+            :onMainSlideClick="void 0"
+            @after-slide-change="onAfterSlideChange"
+            class=""
+          >
+            <slide
+              class="text-center ma-0 pa-0 black"
+              v-for="(card, i) in cards"
+              :index="i"
+              :key="i"
+            >
+              <!-- style="height: 36px; background-color: transparent" -->
+              <!-- class -->
+              <!-- blue-grey darken-4  -->
+              <v-row
+                class="ma-0 pa-0"
+                style="height: 36px"
+                dense
+                no-gutters
+                justify="center"
+                align-content="center"
+              >
+                {{ card.title }}
+              </v-row>
+              <!-- Slide 1 Content -->
+              <!-- <img src="https://placehold.it/360x270" /> -->
+              <!-- src="https://img.youtube.com/vi/ZoXdmxYa90c/default.jpg" -->
+              <v-img
+                v-if="i != slideIndex"
+                width="320"
+                height="180"
+                :lazy-src="require('assets/images/default.jpg')"
+                :src="card.tmb"
+              >
+              </v-img>
+              <LazyYoutubeVideo
+                v-else
+                :src="card.src"
+                :preview-image-size="card.previewImageSize"
+                aspect-ratio="16:9"
+                :thumbnail-listeners="{ load: foo }"
+              />
+            </slide>
+          </carousel-3d>
+        </div>
       </client-only>
+      <v-row class="mr-4 mt-10">
+        <v-spacer />
+        <v-btn color="primary" nuxt to="/gallery">次のページ</v-btn>
+      </v-row>
     </div>
   </div>
 </template>
@@ -159,4 +194,64 @@ export default {
   },
 }
 </script>
+
+
+
+<style lang="scss" scoped>
+$text-color: #eff1ff;
+// $background-color: #04009d;
+$background-color: #666;
+// $background-color-dark: #06004d;
+$background-color-dark: #111;
+.ff7-card {
+  // border: solid 1px #424542;
+  // box-shadow: 1px 1px #e7dfe7, -1px -1px #e7dfe7, 1px -1px #e7dfe7,
+  //   -1px 1px #e7dfe7, 0 -2px #9c9a9c, -2px 0 #7b757b, 0 2px #424542;
+  // padding: 5px 10px;
+  // background: $background-color;
+  background: transparent;
+  background: -moz-linear-gradient(
+    top,
+    $background-color 0%,
+    $background-color-dark 100%
+  );
+  background: -webkit-gradient(
+    linear,
+    left top,
+    left bottom,
+    color-stop(0%, $background-color),
+    color-stop(100%, $background-color-dark)
+  );
+  background: -webkit-linear-gradient(
+    top,
+    $background-color 0%,
+    $background-color-dark 100%
+  );
+  background: -o-linear-gradient(
+    top,
+    $background-color 0%,
+    $background-color-dark 100%
+  );
+  background: -ms-linear-gradient(
+    top,
+    $background-color 0%,
+    $background-color-dark 100%
+  );
+  background: linear-gradient(
+    to bottom,
+    $background-color 0%,
+    $background-color-dark 100%
+  );
+  filter: progid:dximagetransform.microsoft.gradient( startcolorstr='$background-color', endcolorstr='$background-color-dark',gradienttype=0 );
+  -webkit-border-radius: 7px;
+  -moz-border-radius: 7px;
+  border-radius: 7px;
+  * {
+    color: $text-color;
+    text-shadow: 2px 2px #212421, 1px 1px #212021;
+    font-family: Verdana, sans-serif;
+    font-weight: normal;
+  }
+}
+</style>
 
