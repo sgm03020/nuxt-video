67d112498ba145d215a1587c24ace310adda426c
Tsuyoshi Koike <airtk2000@gmail.com>
2020-10-23 18:15:13 +0900

    configからのapi_url読み出し断念

--- plugins/Hasura.js
+++ plugins/Hasura.js
@@ -3,8 +3,8 @@ export default function ({ $http, $config, env }, inject) {
   // console.log(env.API_HASURA_URL)
   // console.log('config.api_url:', $config.api_url)
   const $hasura = $http.create({
-    //prefixUrl: env.API_HASURA_URL,
-    prefixUrl: $config.api_url,
+    prefixUrl: env.API_HASURA_URL,
+    //prefixUrl: $config.api_url,
   })
   // 以下はなるべく設定しないようにする
   //$hasura.setHeader('X-Hasura-Admin-Secret','password')
