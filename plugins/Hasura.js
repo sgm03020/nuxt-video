7fcfc0ad87187ddf7c7d52219b6569d1c35ab1cb
Tsuyoshi Koike <airtk2000@gmail.com>
2020-10-23 16:24:19 +0900

    Hasura.jsでconfig.api_url利用

--- plugins/Hasura.js
+++ plugins/Hasura.js
@@ -1,10 +1,10 @@
-export default function ({ $http, env }, inject) {
-  //export default function ({ $http, $config, env }, inject) {
-    //console.log(env.API_HASURA_URL)
+//export default function ({ $http, env }, inject) {
+export default function ({ $http, $config, env }, inject) {
+  //console.log(env.API_HASURA_URL)
   //console.log($config.api_url)
   const $hasura = $http.create({
-    prefixUrl: env.API_HASURA_URL,
-    // prefixUrl: $config.api_url,
+    //prefixUrl: env.API_HASURA_URL,
+    prefixUrl: $config.api_url,
   })
   // 以下はなるべく設定しないようにする
   //$hasura.setHeader('X-Hasura-Admin-Secret','password')
