6e078a06a907d2f7ff486962bfef6c63253d8f21
Tsuyoshi Koike <airtk2000@gmail.com>
2020-10-26 22:05:06 +0900

    light theme

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,8 +10,8 @@ export default {
     titleTemplate: '%s - nuxt-video',
     title: 'nuxt-video',
     htmlAttrs: {
-      lang: 'ja'
-    },    
+      lang: 'ja',
+    },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -21,13 +21,11 @@ export default {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [
-    'vue-lazy-youtube-video/dist/style.simplified.css'
-  ],
+  css: ['vue-lazy-youtube-video/dist/style.simplified.css'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
-    {src: '~/plugins/Carousel3d', mode:'client'},
+    { src: '~/plugins/Carousel3d', mode: 'client' },
     // {src: '~/plugins/Carousel3d', mode:'server'},
     '@/plugins/Hasura.js',
     '@/plugins/SetInterval.js',
@@ -44,6 +42,8 @@ export default {
     '@nuxtjs/stylelint-module',
     // https://go.nuxtjs.dev/vuetify
     '@nuxtjs/vuetify',
+    // https://color-mode.nuxtjs.org ADD 2020.10.26
+    // '@nuxtjs/color-mode'
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
@@ -57,7 +57,7 @@ export default {
     // add
     '@nuxtjs/proxy',
     // add
-    '@nuxt/http'
+    '@nuxt/http',
   ],
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
@@ -81,6 +81,16 @@ export default {
           error: colors.deepOrange.accent4,
           success: colors.green.accent3,
         },
+        /*以下追加*/
+        light: {
+          primary: '#3f51b5',
+          accent: '#8c9eff',
+          secondary: '#b0bec5',
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: '#b71c1c',
+          success: colors.green.accent3,
+        },
       },
     },
   },
@@ -92,13 +102,13 @@ export default {
       config.module.rules.push({
         test: /\.(graphql|gql)$/,
         exclude: /node_modules/,
-        use: 'graphql-tag/loader'        
+        use: 'graphql-tag/loader',
       })
       // isDev が true の場合、webpack を開発モードに設定します
       if (isDev) {
         //config.mode = 'development'
       }
-    }    
+    },
   },
 
   // env
@@ -106,7 +116,7 @@ export default {
     baseUrl: process.env.BASE_URL || 'http://localhost:3000',
     // API_HASURA_URL: process.env.API_HASURA_URL || 'https://vcollectionbackendtestapi.tk:4430/v1/graphql',
   },
-  
+
   generate: {
     fallback: false,
     routes: [
@@ -119,13 +129,13 @@ export default {
       // '/videos/stretch',
       // '/videos/topics',
       // '/inspire',
-      // '/hasura',      
-      // '/transitions',      
-    ],  
+      // '/hasura',
+      // '/transitions',
+    ],
   },
 
   router: {
-    middleware: 'CollectionMiddleware'
+    middleware: 'CollectionMiddleware',
   },
 
   publicRuntimeConfig: {
@@ -133,13 +143,13 @@ export default {
     // api_url: 'https://11vcollectionbackendtestapi.tk:4430/v1/graphql'
     // api_url: process.env.NODE_ENV !== 'production' ? 'https://vcollectionbackendtestapi.tk:4430/v1/graphql' : undefined
     // api_url: process.env.API_HASURA_URL
-    api_url: process.env.NODE_ENV !== 'production' ? process.env.API_HASURA_URL:''
-  },  
+    api_url:
+      process.env.NODE_ENV !== 'production' ? process.env.API_HASURA_URL : '',
+  },
 
   privateRuntimeConfig: {
     // api_url: API_HASURA_URL
     // api_url: 'https://vcollectionbackendtestapi.tk:4430/v1/graphql'
-    api_url: process.env.API_HASURA_URL
-  }
-  
+    api_url: process.env.API_HASURA_URL,
+  },
 }
