aa8f080a9563cc99dbcee990674f8316c0e6a4e0
Tsuyoshi Koike <airtk2000@gmail.com>
2021-11-13 17:47:34 +0900

    fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,7 +45,7 @@ export default {
     // https://go.nuxtjs.dev/typescript
     '@nuxt/typescript-build',
     // https://go.nuxtjs.dev/stylelint
-    '@nuxtjs/stylelint-module',
+    // '@nuxtjs/stylelint-module',
     // https://go.nuxtjs.dev/vuetify
     '@nuxtjs/vuetify',
     // https://color-mode.nuxtjs.org ADD 2020.10.26
